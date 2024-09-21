e89fdfbb875611da36a205bc290456d58bfdc51f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-09-21 13:42:55 -0300

    add mapstruct

--- src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
@@ -1,8 +1,11 @@
 package guru.springfamework.api.v1.model;
 
-/**
- * Created by jt on 9/24/17.
- */
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
 public class CategoryDTO {
     private Long id;
     private String name;
