a69fadf925d2094d20a3a865c0502337971092f8
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-09-22 11:01:27 -0300

    implement category controller

---
+++ src/main/java/guru/springfamework/api/v1/model/CategoryListDTO.java
@@ -0,0 +1,14 @@
+package guru.springfamework.api.v1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CategoryListDTO {
+    List<CategoryDTO> categories;
+}
