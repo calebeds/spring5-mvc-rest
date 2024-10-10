10abbfbc04542d6357db75216c828a810ddc272b
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-10-10 11:59:55 -0300

    customize swagger metadata

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -3,11 +3,16 @@ package guru.springfamework.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
@@ -18,6 +23,22 @@ public class SwaggerConfig {
                 .select().apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("Calebe", "https://calebe-oliveira.me", "calebe.dso@gmail.com");
+
+        return new ApiInfoBuilder()
+                .title("Spring Mvc Rest")
+                .description("Spring Framework 5")
+                .version("1.0")
+                .termsOfServiceUrl("Terms of Service: blah")
+                .contact(contact)
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .extensions(new ArrayList<>())
+                .build();
     }
 }
